518920a4c3ccfd72b336541ceb2ebd22916b7adc
Ford Leins <fordleins@gmail.com>
2024-02-16 18:31:52 +1100

    Add Archivo font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
         extend: {
+            fontFamily: {
+                customFont: ['"Archivo"', "sans-serif"],
+            },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
             }
