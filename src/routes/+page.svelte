9bba5e71f86bd52e190e9f041969422b960a0fc6
Ford Leins <fordleins@gmail.com>
2024-02-16 17:57:51 +1100

    Radial gradient around mouse

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        window.addEventListener("mousemove", updateMousePos);
+        return () => {
+            window.removeEventListener("mousemove", updateMousePos);
+        };
+    });
+
+    export let mousePos = {
+        x: 0,
+        y: 0,
+    };
+
+    const updateMousePos = (event) => {
+        mousePos = {
+            x: event.clientX,
+            y: event.clientY,
+        };
+
+        const bg = document.getElementById("background");
+        bg.style.setProperty("left", `${mousePos.x}px`);
+        bg.style.setProperty("top", `${mousePos.y}px`);
+    };
+</script>
+
+<div class="-z-10 bg-slate-800 h-screen w-screen fixed flex place-items-center">
+    <div
+        id="background"
+        class="-z-20 w-[100rem] h-[100rem] absolute -translate-x-1/2 -translate-y-1/2 rounded-full bg-gradient-radial from-cyan-950 via-transparent to-transparent"
+    ></div>
+</div>
