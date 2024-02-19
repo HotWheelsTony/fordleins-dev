ae755446b820c76b27b51fc4dd823fd7677c0244
Ford Leins <fordleins@gmail.com>
2024-02-19 12:42:33 +1100

    Rework home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
     import { onMount } from "svelte";
+    import Card from "$lib/card.svelte";
+    import Header from "$lib/header.svelte";
+    import Footer from "$lib/footer.svelte";
 
     onMount(() => {
         window.addEventListener("mousemove", updateMousePos);
@@ -8,26 +11,38 @@
         };
     });
 
-    export let mousePos = {
-        x: 0,
-        y: 0,
-    };
-
     const updateMousePos = (event) => {
-        mousePos = {
-            x: event.clientX,
-            y: event.clientY,
-        };
-
         const bg = document.getElementById("background");
-        bg.style.setProperty("left", `${mousePos.x}px`);
-        bg.style.setProperty("top", `${mousePos.y}px`);
+
+        bg.style.setProperty(
+            "background",
+            `radial-gradient(800px at ${event.clientX}px ${event.clientY}px, rgba(8, 51, 68, 0.5), transparent 70%)`,
+        );
     };
 </script>
 
-<div class="-z-10 bg-slate-800 h-screen w-screen fixed flex place-items-center">
-    <div
-        id="background"
-        class="-z-20 w-[100rem] h-[100rem] absolute -translate-x-1/2 -translate-y-1/2 rounded-full bg-gradient-radial from-cyan-950 via-transparent to-transparent"
-    ></div>
+<div id="background" class="fixed inset-0 -z-10" />
+
+<div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 lg:py-0 lg:px-24">
+    <div class="lg:flex lg:justify-between lg:gap-4">
+        <div class="lg:sticky lg:h-screen lg:top-0 lg:py-24">
+            <Header />
+        </div>
+        <main class="group/cards lg:w-3/5 py-12 lg:py-24">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </main>
+    </div>
 </div>
+
+<style>
+    #background {
+        background: radial-gradient(
+            800px at 0px 0px,
+            rgba(8, 51, 68, 0.5),
+            transparent 50%
+        );
+    }
+</style>
