6161b8d4847226b53fecd6517149c236b5370825
Ford Leins <fordleins@gmail.com>
2024-02-19 12:42:20 +1100

    Extract header

---
+++ src/lib/header.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Icon from "./icon.svelte";
+</script>
+
+<header>
+    <div class="mb-5 lg:mb-10">
+        <h1 class="font-bold text-white text-5xl tracking-tight mb-3">
+            Ford Leins
+        </h1>
+        <span class="font-medium text-white text-xl tracking-tight">
+            Software Developer at SpatialNetworx
+        </span>
+    </div>
+    <div class="flex gap-2">
+        <Icon name="linkedin" />
+        <Icon name="github" />
+    </div>
+</header>
