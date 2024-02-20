d7b8e2fe54c8a520c45c1844ee7d8059a8dfc5c8
Ford Leins <fordleins@gmail.com>
2024-02-20 22:00:55 +1100

    Add url to projects

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -3,21 +3,25 @@
 
     const projects = [
         {
-            title: "Personal Portfolio",
+            title: "fordleins.dev",
             org: "Personal",
             date: "Feb 2024 - Present",
             skills: ["SvelteKit", "TailwindCSS", "Firebase"],
-            description: `A good excuse to dip my toes into SvelteKit, this site is designed to act as a kind of 'living resume'. A place to showcase my experience and personal projects.`,
+            description: `A good excuse to dip my toes into SvelteKit, 
+                this site is designed to act as a kind of 'living resume'. 
+                A place to showcase my experience and personal projects.`,
         },
         {
-            title: "TrailerCo",
+            title: "trailerco.nz",
             org: "TrailerCo Ltd.",
             date: "Jan 2024 - Present",
             skills: ["Angular", "TypeScript", "TailwindCSS", "Firebase"],
-            description: `Helped a new trailer building business establish their online presence with a simple website showcasing their work.`,
+            description: `Helped a new trailer building business establish their 
+                online presence with a simple website showcasing their work.`,
+            url: "https://trailerco.nz/home",
         },
         {
-            title: "Argent Digital",
+            title: "argentdigital.co.nz",
             org: "Argent Digital Ltd.",
             date: "Dec 2023 - Present",
             skills: [
@@ -27,11 +31,13 @@
                 "Firebase",
                 "Google Cloud",
             ],
-            description: `Created a simple portfolio website with contact functionality for a photography & videography in New Zealand.`,
+            description: `Created a simple portfolio website with contact functionality 
+                for a photography & videography in New Zealand.`,
+            url: "https://argentdigital.co.nz/",
         },
         {
-            title: "Essential Therapy Aotearoa",
-            org: "Private Client",
+            title: "essentialtherapyaotearoa.co.nz",
+            org: "Essential Therapy Aotearoa",
             date: "Sep 2023 - Present",
             skills: [
                 "Angular",
@@ -43,17 +49,18 @@
                 "GitHub Actions",
                 "Google Calendar",
             ],
-            description: `Designed and built a bespoke booking platform and product catalogue for a small massage & aromatherapy business.
-            The aim was to allow users to easily make appointments via a streamlined process, and enable my client
-            to manage these bookings easily by integrating directly into their personal Google calendar.`,
+            description: `Designed and built a bespoke booking platform and product 
+                catalogue for a small massage & aromatherapy business. The aim was 
+                to allow users to easily make appointments via a streamlined process, 
+                and enable the client to manage these bookings easily by integrating 
+                directly with their personal Google calendar.`,
+            url: "https://essentialtherapyaotearoa.co.nz/home",
         },
     ];
 </script>
 
-<div>
-    <div class="group/cards">
-        {#each projects as project}
-            <Card content={project} />
-        {/each}
-    </div>
+<div class="group/cards">
+    {#each projects as project}
+        <Card content={project} />
+    {/each}
 </div>
