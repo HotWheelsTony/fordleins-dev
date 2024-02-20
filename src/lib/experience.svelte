a4b41d58ac3fe7bb3135b500b63b9e9b6476e257
Ford Leins <fordleins@gmail.com>
2024-02-20 14:41:23 +1100

    Add more roles

--- src/lib/experience.svelte
+++ src/lib/experience.svelte
@@ -3,6 +3,13 @@
 
     const roles = [
         {
+            title: "Software Developer",
+            org: "SpatialNetworx",
+            date: "Nov 2023 - Present ",
+            skills: ["C#", "Python", "PostGreSQL", "Angular"],
+            description: `Software developer across multiple projects`,
+        },
+        {
             title: "Junior Full Stack Developer",
             org: "Capgemini",
             date: "Feb 2022 - Aug 2023",
@@ -10,6 +17,30 @@
             description: `Worked as part of Capgemini\’s Cloud & Custom Apps team as a junior full stack developer. I contributed to
             diverse range of projects for a number of clients.`,
         },
+        {
+            title: "Software Engineering Tutor",
+            org: "Victoria University of Wellington",
+            date: "Mar 2021 - Nov 2021",
+            skills: [
+                "Java",
+                "Tutoring",
+                "Communication",
+                "IntelliJ",
+                "Algorithms",
+                "Data Structures",
+            ],
+            description: `Worked part time for two semesters as a software engineering tutor. Delivering in person labs to
+    second year software engineering students, marking assignments, and helping with test invigilation.
+    The courses I tutored were focused on implementing algorithms & design patterns, and teaching best
+    practices when writing software.`,
+        },
+        // {
+        //     title: "",
+        //     org: "",
+        //     date: "",
+        //     skills: "",
+        //     description: "",
+        // },
     ];
 </script>
 
