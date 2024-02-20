73490d8db46b296d50042b0c5c438c0ea95bf7e3
Ford Leins <fordleins@gmail.com>
2024-02-20 14:11:11 +1100

    Update card component

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -1,42 +1,42 @@
 <script>
-    export let title = "Role Title";
-    export let company = "Company";
-    export let date = "Jan 2000 - Dec 2000";
-    export let content = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    export let content = {
+        title: "Title",
+        org: "Org",
+        date: "Jan 2000 - Dec 2001",
+        skills: ["React", "Angular"],
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
             veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
             ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
             cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-            id est laborum.`;
-    export let skills = ["React", "Angular"];
+            id est laborum.`,
+    };
 </script>
 
 <div
-    class="lg:hover:!opacity-100 lg:group-hover/cards:opacity-50 mb-4 py-4 lg:px-4 transition sm:flex gap-4 rounded-md lg:hover:drop-shadow-md
+    class="lg:hover:!opacity-100 lg:group-hover/cards:opacity-50 mb-4 py-4 lg:px-4 transition sm:flex gap-6 rounded-md lg:hover:drop-shadow-md
      lg:hover:bg-mediumGrey/40
       lg:hover:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.15)]"
 >
-    <div class="mr-4">
+    <div class="">
         <span class="text-nowrap">
-            {date}
+            {content.date}
         </span>
     </div>
     <div>
-        <h2>
-            {title} &bull; {company}
+        <h2 class="text-lg">
+            {content.title} &bull; {content.org}
         </h2>
         <p>
-            {content}
+            {content.description}
         </p>
-        <div class="flex my-1">
-            {#each skills as skill}
+        <div class="flex flex-wrap">
+            {#each content.skills as skill}
                 <div
-                    class="text-sm rounded-full bg-green/10 w-fit mr-2 px-3 py-1.5"
+                    class="mr-2 mt-2 px-2 text-sm text-green bg-green/10 rounded-full"
                 >
-                    <span class="text-green">
-                        {skill}
-                    </span>
+                    {skill}
                 </div>
             {/each}
         </div>
