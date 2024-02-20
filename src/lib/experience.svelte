d561c062c210fee5bcfd244d6860208a782d6021
Ford Leins <fordleins@gmail.com>
2024-02-20 14:11:44 +1100

    Flesh out experience, projects, and skills components

---
+++ src/lib/experience.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Card from "./card.svelte";
+
+    const roles = [
+        {
+            title: "Junior Full Stack Developer",
+            org: "Capgemini",
+            date: "Feb 2022 - Aug 2023",
+            skills: ["Angular", "TypeScript", "SQL", "C#", "Docker"],
+            description: `Worked as part of Capgemini\’s Cloud & Custom Apps team as a junior full stack developer. I contributed to
+            diverse range of projects for a number of clients.`,
+        },
+    ];
+</script>
+
+<div>
+    <div class="group/cards">
+        {#each roles as role}
+            <Card content={role} />
+        {/each}
+    </div>
+</div>
---
+++ src/lib/projects.svelte
@@ -0,0 +1,33 @@
+<script>
+    import Card from "./card.svelte";
+
+    const projects = [
+        {
+            title: "Essential Therapy Aotearoa",
+            org: "Private Client",
+            date: "Sep 2023 - Present",
+            skills: [
+                "Angular",
+                "Ionic",
+                "CSS",
+                "TypeScript",
+                "Google Cloud Functions",
+                "Sendgrid API",
+                "Firebase",
+                "GitHub",
+                "Google Calendar API",
+            ],
+            description: `Bespoke booking platform and simple storefront for a small massage clinic business, currently in MVP.
+            Designed to allow users to easily make appointments via a streamlined process, and enable my client
+            to manage these bookings easily in their Google calendar.`,
+        },
+    ];
+</script>
+
+<div>
+    <div class="group/cards">
+        {#each projects as project}
+            <Card content={project} />
+        {/each}
+    </div>
+</div>
