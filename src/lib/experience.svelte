8c4cdb6b83f1580e3c5e463cfcb07f5690979412
Ford Leins <fordleins@gmail.com>
2024-02-21 14:15:47 +1100

    Add links to experiences

--- src/lib/experience.svelte
+++ src/lib/experience.svelte
@@ -8,14 +8,17 @@
             date: "Nov 2023 - Present ",
             skills: ["C#", "Python", "PostGreSQL", "Angular"],
             description: `Software developer across multiple projects`,
+            url: "https://spatialnetworx.ai/",
         },
         {
             title: "Junior Full Stack Developer",
             org: "Capgemini",
             date: "Feb 2022 - Aug 2023",
             skills: ["Angular", "TypeScript", "SQL", "C#", "Docker"],
-            description: `Worked as part of Capgemini\’s Cloud & Custom Apps team as a junior full stack developer. I contributed to
-            diverse range of projects for a number of clients.`,
+            description: `Worked as part of Capgemini\’s Cloud & Custom Apps team 
+                as a junior full stack developer. I contributed to diverse range 
+                of projects for a number of clients.`,
+            url: "https://www.capgemini.com/",
         },
         {
             title: "Software Engineering Tutor",
@@ -29,10 +32,12 @@
                 "Algorithms",
                 "Data Structures",
             ],
-            description: `Worked part time for two semesters as a software engineering tutor. Delivering in person labs to
-    second year software engineering students, marking assignments, and helping with test invigilation.
-    The courses I tutored were focused on implementing algorithms & design patterns, and teaching best
-    practices when writing software.`,
+            description: `Worked part time for two semesters as a software engineering 
+                tutor. Delivering in person labs to second year software engineering 
+                students, marking assignments, and helping with test invigilation.
+                The courses I tutored were focused on implementing algorithms & design 
+                patterns, and teaching best practices when writing software.`,
+            url: "https://www.wgtn.ac.nz/",
         },
     ];
 </script>
