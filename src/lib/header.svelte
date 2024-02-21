d21ac79b63ebce6c92783fee1c1dede4462be611
Ford Leins <fordleins@gmail.com>
2024-02-21 20:08:41 +1100

    Add about blurb to header

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,9 +1,13 @@
 <script>
     import Icon from "./icon.svelte";
+
+    const about = `Nearly 3 years of professional experience in software engineering, across tutoring
+and full stack development positions. My full stack experience was gained while
+working for Capgemini NZ, during my time there I worked on a number of projects.`;
 </script>
 
-<header>
-    <div class="mb-5 lg:mb-10">
+<header class="mb-6">
+    <div class="mb-6 lg:mb-10">
         <h1 class="text-5xl mb-3">Ford Leins</h1>
         <span class="text-xl">
             Software Developer at <a href="https://spatialnetworx.ai/"
@@ -11,7 +15,7 @@
             >
         </span>
     </div>
-    <div class="flex gap-2">
+    <div class="flex gap-2 mb-6 lg:mb-10">
         <a href="https://www.linkedin.com/in/ford-leins/">
             <Icon name="linkedin" />
         </a>
@@ -21,5 +25,10 @@
         <a href="mailto:fordleins@gmail.com">
             <Icon name="email" />
         </a>
+    </div>
+    <div class="md:w-3/5 mb-6">
+        <span class="text-lg">
+            {about}
+        </span>
     </div>
 </header>
