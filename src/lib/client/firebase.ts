a0f13c5bd3cc8605c92ea875d70f8c425fad80a7
Ford Leins <fordleins@gmail.com>
2024-02-22 10:02:35 +1100

    Set up firebase and static site build for firebase hosting

---
+++ src/lib/client/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1UJ5nqYekWUCHbAgN2VESbTdP2b8FY7Y",
+    authDomain: "fordleins-dev.firebaseapp.com",
+    projectId: "fordleins-dev",
+    storageBucket: "fordleins-dev.appspot.com",
+    messagingSenderId: "111803429573",
+    appId: "1:111803429573:web:af744cb11b24b010e776ac"
+};
+
+const app = initializeApp(firebaseConfig);
