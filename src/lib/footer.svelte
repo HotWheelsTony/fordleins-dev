f46e20e597dc207ee591da1ecae710bfa726fc4f
Ford Leins <fordleins@gmail.com>
2024-02-21 20:07:55 +1100

    Set footer width and margin

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -2,29 +2,27 @@
     import Icon from "./icon.svelte";
 </script>
 
-<footer>
-    <div class="w-2/3">
-        <span class="text-lg">
-            Built with
-            <a href="https://kit.svelte.dev/">SvelteKit,</a>
-            <a href="https://tailwindcss.com/">Tailwind,</a>
-            <a href="https://firebase.google.com/">Firebase,</a>
-            and
-            <a href="https://code.visualstudio.com/">Visual Studio Code</a>.
-        </span>
-        <div class="flex mt-2 gap-2">
-            <a class="icon" href="https://kit.svelte.dev/">
-                <Icon name="svelte" />
-            </a>
-            <a class="icon" href="https://tailwindcss.com/">
-                <Icon name="tailwind" />
-            </a>
-            <a class="icon" href="https://firebase.google.com/">
-                <Icon name="firebase" />
-            </a>
-            <a class="icon" href="https://code.visualstudio.com/">
-                <Icon name="vscode" />
-            </a>
-        </div>
+<footer class="sm:w-3/5 mt-24">
+    <span class="text-lg">
+        Built with
+        <a href="https://kit.svelte.dev/">SvelteKit,</a>
+        <a href="https://tailwindcss.com/">Tailwind,</a>
+        <a href="https://firebase.google.com/">Firebase,</a>
+        and
+        <a href="https://code.visualstudio.com/">Visual Studio Code</a>.
+    </span>
+    <div class="flex mt-2 gap-2">
+        <a class="icon" href="https://kit.svelte.dev/">
+            <Icon name="svelte" />
+        </a>
+        <a class="icon" href="https://tailwindcss.com/">
+            <Icon name="tailwind" />
+        </a>
+        <a class="icon" href="https://firebase.google.com/">
+            <Icon name="firebase" />
+        </a>
+        <a class="icon" href="https://code.visualstudio.com/">
+            <Icon name="vscode" />
+        </a>
     </div>
 </footer>
