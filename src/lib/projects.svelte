57a98fe74cd6a11700dff46bb33cf9f248675687
Ford Leins <fordleins@gmail.com>
2024-02-22 16:19:33 +1100

    Reorder projects, add one up client

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -4,40 +4,6 @@
 
     const projects = [
         {
-            title: "fordleins.dev",
-            org: "Personal",
-            date: "Feb 2024",
-            skills: ["SvelteKit", "TypeScript", "TailwindCSS", "Firebase"],
-            description: `A good excuse to dip my toes into SvelteKit, 
-                this site is designed to act as a kind of 'living resume'. 
-                A place to showcase my experience and projects.`,
-            url: "https://github.com/HotWheelsTony/fordleins-dev",
-        },
-        {
-            title: "trailerco.nz",
-            org: "TrailerCo Ltd.",
-            date: "Jan 2024",
-            skills: ["Angular", "TypeScript", "TailwindCSS", "Firebase"],
-            description: `Helped a new trailer building business establish their 
-                online presence with a simple website showcasing their work.`,
-            url: "https://trailerco.nz/home",
-        },
-        {
-            title: "argentdigital.co.nz",
-            org: "Argent Digital Ltd.",
-            date: "Dec 2023",
-            skills: [
-                "Angular",
-                "TypeScript",
-                "Less CSS",
-                "Firebase",
-                "Google Cloud",
-            ],
-            description: `Created a simple portfolio website with contact functionality 
-                for a photography & videography business in New Zealand.`,
-            url: "https://argentdigital.co.nz/",
-        },
-        {
             title: "essentialtherapyaotearoa.co.nz",
             org: "Essential Therapy Aotearoa",
             date: "Sep 2023",
@@ -57,6 +23,48 @@
                 and enable the client to manage these bookings easily by integrating 
                 directly with Google calendar.`,
             url: "https://essentialtherapyaotearoa.co.nz/home",
+        },
+        {
+            title: "fordleins.dev",
+            org: "Personal",
+            date: "Feb 2024",
+            skills: ["SvelteKit", "TypeScript", "TailwindCSS", "Firebase"],
+            description: `A good excuse to dip my toes into SvelteKit, 
+                this site is designed to act as a kind of 'living resume'. 
+                A place to showcase my experience and projects.`,
+            url: "https://github.com/HotWheelsTony/fordleins-dev",
+        },
+        {
+            title: "argentdigital.co.nz",
+            org: "Argent Digital Ltd.",
+            date: "Dec 2023",
+            skills: [
+                "Angular",
+                "TypeScript",
+                "Less CSS",
+                "Firebase",
+                "Google Cloud",
+            ],
+            description: `Created a simple portfolio website with contact functionality 
+                for a photography & videography business in New Zealand.`,
+            url: "https://argentdigital.co.nz/",
+        },
+        {
+            title: "trailerco.nz",
+            org: "TrailerCo Ltd.",
+            date: "Jan 2024",
+            skills: ["Angular", "TypeScript", "TailwindCSS", "Firebase"],
+            description: `Helped a new trailer building business establish their 
+                online presence with a simple website showcasing their work.`,
+            url: "https://trailerco.nz/home",
+        },
+        {
+            title: "One Up Client",
+            org: "Personal",
+            date: "Aug 2023",
+            skills: ["Angular", "TypeScript", "Ionic", "Capacitor", "CSS"],
+            description: `Developed a lightweight personal banking client for quickly checking balances and searching transactions using Up bank's public API. `,
+            url: "https://github.com/HotWheelsTony/one-up-client",
         },
     ];
 </script>
