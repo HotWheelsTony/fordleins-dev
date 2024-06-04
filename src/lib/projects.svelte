075bffb70c06224929801af2b54fcb02ef6ec0fa
Ford Leins <fordleins@gmail.com>
2024-06-04 13:47:57 +1000

    Update projects

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -8,6 +8,7 @@
             org: "Essential Therapy Aotearoa",
             date: "Sep 2023",
             skills: [
+                "Stripe",
                 "Angular",
                 "Ionic",
                 "Less CSS",
@@ -17,11 +18,12 @@
                 "GitHub Actions",
                 "Google Calendar",
             ],
-            description: `Designed and built a bespoke booking platform and product 
-                catalogue for a massage & aromatherapy business. The aim was 
-                to allow users to easily make appointments via a streamlined process, 
-                and enable the client to manage these bookings easily by integrating 
-                directly with Google calendar.`,
+            description: `Designed and built a bespoke booking platform and online store
+                for a small massage & aromatherapy business. The aim was to bring the clients business
+                online for as little ongoing cost as possible, while also creating a great user experience. 
+                This was achieved by utilising free tiers of Google Cloud and Firebase for deployment and Stripe
+                for processing payments, which brought the annual cost of the project down to just the cost of 
+                the domain and Stripe's per-transaction fee.`,
             url: "https://essentialtherapyaotearoa.co.nz/home",
         },
         {
@@ -35,34 +37,10 @@
             url: "https://github.com/HotWheelsTony/fordleins-dev",
         },
         {
-            title: "argentdigital.co.nz",
-            org: "Argent Digital Ltd.",
-            date: "Dec 2023",
-            skills: [
-                "Angular",
-                "TypeScript",
-                "Less CSS",
-                "Firebase",
-                "Google Cloud",
-            ],
-            description: `Created a simple portfolio website with contact functionality 
-                for a photography & videography business in New Zealand.`,
-            url: "https://argentdigital.co.nz/",
-        },
-        {
-            title: "trailerco.nz",
-            org: "TrailerCo Ltd.",
-            date: "Jan 2024",
-            skills: ["Angular", "TypeScript", "TailwindCSS", "Firebase"],
-            description: `Helped a new trailer building business establish their 
-                online presence with a simple website showcasing their work.`,
-            url: "https://trailerco.nz/home",
-        },
-        {
             title: "One Up Client",
             org: "Personal",
             date: "Aug 2023",
-            skills: ["Angular", "TypeScript", "Ionic", "Capacitor", "CSS"],
+            skills: ["Angular", "TypeScript", "Capacitor", "CSS"],
             description: `Developed a lightweight personal banking client for quickly checking balances and searching transactions using Up bank's public API. `,
             url: "https://github.com/HotWheelsTony/one-up-client",
         },
